77ccb2001a0b5f9c924889a0f8a719eb8f9a23ce
ANDRES.GUTIERREZCORALES@ucr.ac.cr <ANDRES.GUTIERREZCORALES@ucr.ac.cr>
2024-07-03 16:22:20 -0600

    Cambios del Seeder

--- database/seeders/MaterialTableSeeder.php
+++ database/seeders/MaterialTableSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Categoria;
+use App\Models\Material;
 
 
 class MaterialTableSeeder extends Seeder
@@ -14,6 +14,6 @@ class MaterialTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Categoria::factory()->create();
+        Material::factory()->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,14 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-<<<<<<< HEAD
-Route::post('/material', [MaterialController::class, 'store']);
-Route::put('/material/{codigo}', [MaterialController::class, 'update']);
-Route::get('/material', [MaterialController::class, 'index']);
-=======
-
-use App\Http\Controllers\MaterialController;
-
 Route::post('/material', [MaterialController::class, 'store']);
 
->>>>>>> 5cbe3381800167d81a02b54332a18d8379d8a5fc
