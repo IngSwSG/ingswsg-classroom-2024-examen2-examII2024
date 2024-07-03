747ee2e6b52660531f85676ce02ba22c494eeb52
SEBASTIAN.LOPEZORTIZ <SEBASTIAN.LOPEZORTIZ@ucr.ac.cr>
2024-07-03 17:08:54 -0600

    Sebastian: Clase Unidad y MaterialUnidad

---
+++ app/Models/MaterialUnidad.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MaterialUnidad extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['cantidad', 'idUnidad']; 
+    
+
+    public function unidad()
+
+  { 
+
+    return $this->belongsTo(Unidad::class, 'idUnidad'); 
+  }
+
+}
