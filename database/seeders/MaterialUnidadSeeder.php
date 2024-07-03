a6138734b9c50a28977d414ae90395c1f8babd86
33Enrique <enrique.gomezdu@gmail.com>
2024-07-03 14:30:11 -0600

    Creando los modelos, seeders, factories y migraciones de cada clase

---
+++ database/seeders/MaterialUnidadSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MaterialUnidadSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
