828e29f7f07e8ad34461e7ed86aa7edb8c38f12f
ANDRES.GUTIERREZCORALES@ucr.ac.cr <ANDRES.GUTIERREZCORALES@ucr.ac.cr>
2024-07-03 15:31:19 -0600

    Arreglo de migracion requisiciones

--- database/migrations/2024_07_03_212735_presupuestos.php
+++ database/migrations/2024_07_03_212735_presupuestos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePresupuestosTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -22,4 +22,4 @@ class CreatePresupuestosTable extends Migration
     {
         Schema::dropIfExists('presupuestos');
     }
-}
+};
