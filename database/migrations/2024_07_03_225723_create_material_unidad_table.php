e893458186c9fed757556ac813dede2475b79bb7
ANDRES.GUTIERREZCORALES@ucr.ac.cr <ANDRES.GUTIERREZCORALES@ucr.ac.cr>
2024-07-03 18:18:56 -0600

    Modificacion de migracion materialUnidad

--- database/migrations/2024_07_03_225723_create_material_unidad_table.php
+++ database/migrations/2024_07_03_225723_create_material_unidad_table.php
@@ -9,18 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('material_unidad', function (Blueprint $table) {
-            $table->id();
+            $table->id('idMaterialUnidad');
+            $table->integer('cantidad');
+            $table->unsignedBigInteger('idUnidad');
+            $table->unsignedBigInteger('idMaterial');
             $table->timestamps();
+
+            $table->foreign('idUnidad')->references('idUnidad')->on('unidades')->onDelete('cascade');
+            $table->foreign('idMaterial')->references('codigo')->on('materiales')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('material_unidad');
     }
